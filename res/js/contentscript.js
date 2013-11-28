b1a013492253de148bba920941c5f4b095b879e8
Colten Rouska <crouska@gmail.com>
2013-11-27 17:59:14 -0700

    Done?

--- res/js/contentscript.js
+++ res/js/contentscript.js
@@ -183,32 +183,44 @@ extension.Toolz.parseDate = function(item){
 }
 
 designer.addViewItem = function(item){
-  designer.searchBody.append("<div id='"+item.id+"' class='search-item' alt='"+item.description+"'><img src='"+item.icon+"'/><h3>"+item.name+"</h3><img style='display: none;' src='star-off.png'/><div id='search-item-content' style='display: none;' class='search-item-content'><hr/><table class='item-prices'><thead><tr><td>Time</td><td>Price/Change</td></tr></thead><tbody><tr><td>Today's Price</td><td>"+item.current.formated_price+"</td></tr><tr><td>Today's Change</td><td>"+item.today.price+"</td></tr></tbody></table><div id='graph"+item.id+"' class='graph width='300' height='300' ></div></div></div>");
+  designer.searchBody.append("<div id='"+item.id+"' class='search-item' alt='"+item.description+"'><img src='"+item.icon+"'/><h3>"+item.name+"</h3><img style='display: none;' src='star-off.png'/><div id='search-item-content' style='display: none;' class='search-item-content'><hr/><table class='item-prices'><thead><tr><td>Time</td><td>Price/Change</td></tr></thead><tbody><tr><td>Today's Price</td><td>"+item.current.formated_price+"</td></tr><tr><td>Today's Change</td><td>"+item.today.price+"</td></tr></tbody></table><div id='graph"+item.id+"' class='graph' ><img src='loading.gif' class='loading'/></div></div></div>");
   
   $('div[id='+item.id+']').click(function(){
     var div = $(this).find('div');
     if (div.css('display') !== "block"){
       var id = parseInt($(this).attr('id'));
-      console.log('querying');
       div.slideDown(500);
       query.itemGraph(id, function(data){
         var daily = [];
         var avg = [];
-        // var prices = [];
         for(var key in data.daily){
           value = data.daily[key];
-          date = moment(parseInt(key)).valueOf()/1000;
+          date = moment(parseInt(key));
           daily.push([date, value]);
-          // prices.push(value);
         }
         for(var key in data.average){
           value = data.average[key];
-          date = moment(parseInt(key)).valueOf()/1000;
+          date = moment(parseInt(key));
           avg.push([date,value]);
         }
-        console.log(daily);
-        $.plot($('#graph'+id), [daily, avg], {
-          xaxis: { mode: "time" }
+        $.plot($('#graph'+id), [
+        {
+          label: "daily",
+          data: daily,
+          hoverable: true
+        }, 
+        {
+          label: "average",
+          data: avg,
+          hoverable: true
+        }], {
+          legend:{
+            position: "nw"
+          },
+          xaxis: { 
+            mode: "time",
+            timeformat: "%m/%d"
+             }
         });
         $('#graph'+id).css('background', 'white')
         $('#graph'+id).show();
@@ -218,59 +230,6 @@ designer.addViewItem = function(item){
       div.slideUp(500);
   });
 }
-// extension.Designer.addToTable = function(data){
-//   extension.Designer.table.append("<tr id='"+data.id+"'><td><img src='"+ data.icon + "'/></td><td>" + data.name + "</td><td>" + data.description + "</td><td>" + data.today.price + "</td></tr>");
-//   $("tr[id='"+data.id+"']").on('click', function(){
-//     id = parseInt($(this).attr('id'));
-//     iManager.getItem(id, function(item){
-//       if(item){
-//         designer.showItemDetails(item);
-//       }
-//       else
-//         query.item(item.id, function(queried){
-//           designer.showItemDetails(queried);
-//         });
-//     });
-//   });
-// }
-// extension.Designer.addMultiple = function(items){
-//   if(items)
-//     for (var i = 0; i < items.length; i++) {
-//       extension.Designer.addToTable(items[i]);
-//     };
-// }
-// extension.Designer.showItemDetails = function(item){
-//   // <!--<tr><td>30 Day Change</td><td>"+item.day30.change+"</td></tr><tr><td>90 Day Change</td><td>"+item.day90.change+"</td></tr><tr><td>180 Day Change</td><td>"+item.day180.change+"</td></tr>-->
-//   $('#item-head').html("<div class='item-header'><h1>"+item.name+"</h1><img src='"+item.icon_large+"' /><table><caption><h2>Price info</h2></caption><tbody><tr><td>Current</td><td>"+item.current.formated_price+"</td></tr><tr><td>Today's Change</td><td>"+item.today.price+"</td></tr></tbody></table></div>");
-//   $.plot($('#item-graph'), [ [[moment().subtract('days', 1).valueOf(), 0], [moment().valueOf(), parseInt(item.current.price)]] ], 
-//   {
-//     yaxis: { max: parseInt(item.current.price)},
-//     mode: "time"
-//   });
-//   $('#search-content').hide();
-//   $('#item').show();
-// }
-
-// extension.ItemManager.addItem = function(item){
-//   chrome.storage.local.set(item);
-// }
-
-
-// extension.ItemManager.getItem = function(item, cllbk){
-//   if(typeof item === "integer")
-//     search = item;
-//   else if(typeof item === "object")
-//     search = item.id
-//   chrome.storage.local.get(search, function(rtrn){
-//     cllbk(rtrn);
-//   });
-// }
-
-// extension.ItemManager.getAll = function(cllbk){
-//   chrome.storage.local.get(null, function(items){
-//     cllbk(items);
-//   });
-// }
 
 $(document).ready(function(){
   extension.Designer.searchBody = $('#search-content');
